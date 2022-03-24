8c3738c4d8f667ea616250a4b970686cb0b3e24b
Laurent Paoletti <lp@providenz.fr>
2022-03-24 14:56:57 +0000

    Add bsdasri synthesis UI

--- front/src/common/fragments.ts
+++ front/src/common/fragments.ts
@@ -402,6 +402,16 @@ const dasriReceptionWasteDetailsFragment = gql`
     }
   }
 `;
+const initialDasriFragment = gql`
+  fragment InitialDasriFragment on InitialBsdasri {
+    id
+    quantity
+    volume
+    weight
+    takenOverAt
+    postalCode
+  }
+`;
 
 const wasteAcceptationFragment = gql`
   fragment WasteAcceptationFragment on BsdasriWasteAcceptation {
@@ -444,6 +454,9 @@ export const dashboardDasriFragment = gql`
       }
     }
     grouping {
+      id
+    }
+    synthesizing {
       id
     }
     createdAt
@@ -547,6 +560,12 @@ export const fullDasriFragment = gql`
       }
     }
     grouping {
+      ...InitialDasriFragment
+    }
+    synthesizing {
+      ...InitialDasriFragment
+    }
+    synthesizedIn {
       id
     }
     createdAt
@@ -561,6 +580,7 @@ export const fullDasriFragment = gql`
   ${dasriTransportWasteDetailsFragment}
   ${dasriReceptionWasteDetailsFragment}
   ${wasteAcceptationFragment}
+  ${initialDasriFragment}
 `;
 
 export const vhuFragment = gql`
